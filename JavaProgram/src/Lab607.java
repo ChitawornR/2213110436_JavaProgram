4cd704f710861adf3cd791e6dd5b67328fa38532
ChitawornR <chitaworn.rr@gmail.com>
2022-12-17 16:12:09 +0700

    Lab607

--- JavaProgram/src/Lab607.java
+++ JavaProgram/src/Lab607.java
@@ -3,17 +3,47 @@ public class Lab607 {
 
 	static String month[] = { "January", "February", "March", "April", "May", "June", "July", "August", "September","October", "November", "December" };
 	static int count[] = new int[12];
-	
+
 	public static void main(String[] args) {
 		inputBirthDate();
 
 	}
 	public static void inputBirthDate() {
 		Scanner scan = new Scanner(System.in);
+		int bmonth;
+		String birtDate;
 		for(int i=0;i<5;i++) {
-				System.out.print("Enter BirthDate :");
-				
+			do {
+				System.out.print("Enter BirthDate "+(i+1)+" :");
+				birtDate = scan.next();
+			}while(!CheckDigit(birtDate));
+			bmonth = checkBirthDate(birtDate);
+			count[bmonth]++;
 		}
+		displayCountBday(count);
+	}
+	
+	public static int checkBirthDate(String bd) {
+		int month = Integer.parseInt(bd.substring(3, 5));
+		return month-1;
+	}
+	
+	public static void displayCountBday(int[] count) {
+		System.out.println();
+		for(int i=0; i<count.length; i++) {
+			System.out.println(month[i]+"  [ "+count[i]+" people]");
+		}
+	}
+	
+	public static boolean CheckDigit(String chk) {
+		int n = 0;
+		if(chk.length() !=10  || chk.charAt(2) != '-' || chk.charAt(5) != '-') return false;
+		String mes = chk.replace("-", "");
+		for(int i=0; i<mes.length(); i++) {
+			if(Character.isDigit(mes.charAt(i))) n++;
+		}
+		return (n == mes.length())? true:false;
+		
 	}
 
 }
