99d37f0c7a77fe69457a396572a2c9018b648f27
ChitawornR <chitaworn.rr@gmail.com>
2023-01-26 12:56:05 +0700

    Attendance[26-Jan-2023]

--- OOP_Program/src/AuthorDemo2.java
+++ OOP_Program/src/AuthorDemo2.java
@@ -5,6 +5,7 @@ public class AuthorDemo2 {
 		Scanner scan = new Scanner(System.in);
 		String[] name = new String[4];
 		String[] email = new String[4];
+		Author[] at = new Author[4];
 		for(int i=0; i<name.length; i++) {
 			System.out.println("Information Author : "+(i+1));
 			System.out.println("------------------------------------------------");
@@ -12,13 +13,14 @@ public class AuthorDemo2 {
 			name[i] = scan.nextLine();
 			System.out.print("Input author email :");
 			email[i] = scan.nextLine();
+			at[i] = new Author(name[i],email[i]);
 			System.out.println("------------------------------------------------");
 		}
 		System.out.println();
-		Author[] at = new Author[4];
-		for(int i=0; i<name.length;i++) {
-			at[i] = new Author(name[i],email[i]);
-			System.out.println((i+1)+". "+at[i].getName()+" ("+at[i].getEmail()+")");
+		int i=1;
+		for(Author _at:at) {
+			System.out.println(i+". "+_at.getName()+" ("+_at.getEmail()+")");
+			i++;
 		}
 	}
 }
